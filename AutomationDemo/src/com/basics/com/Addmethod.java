bbd291f01af75fad03c177668b0142ff42c0be8e
LENOVO <saijitendra03@gmail.com>
2024-03-28 17:42:01 +0530

    Commit

---
+++ AutomationDemo/src/com/basics/com/Addmethod.java
@@ -0,0 +1,13 @@
+package com.basics.com;
+
+public class Addmethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=4;
+		System.out.println(x+y);
+
+	}
+
+}
